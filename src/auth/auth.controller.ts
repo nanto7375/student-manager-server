b5c6c0b26b09bd1ba99e63464264d5ab4110f0d4
seungwoo <jsu7375@gmail.com>
2025-03-14 02:13:05 +0900

    REFACTOR: auth controller에 있던 비즈니스 로직 auth service로 이동

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,16 +3,13 @@ import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Throttle, ThrottlerGuard } from '@nestjs/throttler';
 import { CookieOptions, Request, Response } from 'express';
 
-import { Forbidden, Unauthorized } from '@src/common/exception/definition.exception';
+import { Unauthorized } from '@src/common/exception/definition.exception';
 import { MyLogger } from '@src/configs/logger/my-logger';
-import { AuthService, JwtPayload, TokenType } from './auth.service';
+import { AuthService } from './auth.service';
 
 import { toInstance } from '@src/common/toInstance';
 import { SigninRequestDto } from './dto/auth-request.dto';
 import { AdminDto } from '@src/admin/dto/admin-response.dto';
-import { Admin } from '@src/admin/entity.ts/admin.entity';
-import { ConfigService } from '@nestjs/config';
-import { FailedSigninAttemptsCache } from './failed-signin-attempts-cache';
 import { getFingerprint } from '@src/common/utils/etc';
 
 // TODO: auth용 throttler 따로 설정하기
@@ -20,31 +17,19 @@ import { getFingerprint } from '@src/common/utils/etc';
 @Controller('auth')
 @UseGuards(ThrottlerGuard)
 export class AuthController {
-  private readonly _ACCESS_TOKEN_EXPIRE_TIME_IN_SECONDS: number;
-  private readonly _REFRESH_TOKEN_EXPIRE_TIME_IN_SECONDS: number;
-
   constructor(
     private readonly authService: AuthService,
     private readonly logger: MyLogger,
-    private readonly configService: ConfigService,
-    private readonly failedSigninAttemptsCache: FailedSigninAttemptsCache,
   ) {
     this.logger.setContext('AuthController');
-    this._ACCESS_TOKEN_EXPIRE_TIME_IN_SECONDS = this.configService.get('SM_JWT_ACCESS_EXPIRE_TIME');
-    this._REFRESH_TOKEN_EXPIRE_TIME_IN_SECONDS = this.configService.get('SM_JWT_REFRESH_EXPIRE_TIME');
   }
 
-  private _getTokenCookieOptions(tokenType: TokenType): CookieOptions {
-    const maxAgeInSeconds =
-      tokenType === TokenType.ACCESS //
-        ? this._ACCESS_TOKEN_EXPIRE_TIME_IN_SECONDS
-        : this._REFRESH_TOKEN_EXPIRE_TIME_IN_SECONDS;
-
+  private _getTokenCookieOptions(exp: number): CookieOptions {
     return {
       httpOnly: true,
       secure: true,
       sameSite: 'none', // domain 설정되면 'strict'로 변경
-      maxAge: maxAgeInSeconds * 1000,
+      maxAge: exp * 1000,
       path: '/',
       // TODO: domain 설정
     };
@@ -55,40 +40,11 @@ export class AuthController {
   @ApiOperation({ summary: '로그인' })
   @ApiOkResponse({ type: AdminDto })
   async signin(@Body() body: SigninRequestDto, @Req() req: Request, @Res({ passthrough: true }) res: Response) {
-    const failedAttempts = await this.failedSigninAttemptsCache.get(body.email);
-    if (failedAttempts >= 5) throw new Forbidden('Too many failed login attempts');
+    const { admin, accessTokenInfo, refreshTokenInfo } = await this.authService.signin({ email: body.email, password: body.password, ip: req.ip, fingerprint: getFingerprint(req) });
 
-    let admin: Admin;
-    try {
-      admin = await this.authService.authenticate(body.email, body.password);
-    } catch (e) {
-      this.logger.warn({ message: 'authentication failed', ip: req.ip });
-      await this.failedSigninAttemptsCache.set(body.email, (failedAttempts || 0) + 1);
-      throw e;
-    }
-
-    const [accessToken, refreshToken] = await Promise.all([
-      this.authService.signJwt({
-        id: admin.id,
-        role: admin.role,
-        exp: this._ACCESS_TOKEN_EXPIRE_TIME_IN_SECONDS,
-        fingerprint: getFingerprint(req),
-      }), //
-      this.authService.signJwt(
-        {
-          id: admin.id,
-          role: admin.role,
-          exp: this._REFRESH_TOKEN_EXPIRE_TIME_IN_SECONDS,
-          fingerprint: getFingerprint(req),
-        },
-        TokenType.REFRESH,
-      ),
-    ]);
-
-    if (failedAttempts > 0) await this.failedSigninAttemptsCache.clear(body.email);
     // TODO: 쿠키 이름을 __Host- prefix를 사용하여 변경하는 것을 고려하세요 (예: __Host-acc, __Host-refr).
-    res.cookie('acc', accessToken, this._getTokenCookieOptions(TokenType.ACCESS));
-    res.cookie('refr', refreshToken, this._getTokenCookieOptions(TokenType.REFRESH));
+    res.cookie('acc', accessTokenInfo.token, this._getTokenCookieOptions(accessTokenInfo.exp));
+    res.cookie('refr', refreshTokenInfo.token, this._getTokenCookieOptions(refreshTokenInfo.exp));
     return toInstance(AdminDto, admin);
   }
 
@@ -115,31 +71,12 @@ export class AuthController {
       throw new Unauthorized();
     }
 
-    if (await this.authService.isDiscardedToken(refreshToken)) {
-      this.logger.warn({ message: 'refresh token is discarded', ip: req.ip });
-      await this.authService.banIp(req.ip as string);
-      throw new Forbidden();
+    const { accessTokenInfo, refreshTokenInfo } = await this.authService.refresh({ refreshToken, ip: req.ip, fingerprint: getFingerprint(req) });
+
+    res.cookie('acc', accessTokenInfo.token, this._getTokenCookieOptions(accessTokenInfo.exp));
+    if (refreshToken !== refreshTokenInfo.token) {
+      res.cookie('refr', refreshTokenInfo.token, this._getTokenCookieOptions(refreshTokenInfo.exp));
     }
-
-    let payload: JwtPayload;
-    try {
-      payload = await this.authService.verifyJwt(refreshToken, TokenType.REFRESH);
-      if (payload.fingerprint !== getFingerprint(req)) throw Error('fingerprint mismatch');
-    } catch (e) {
-      this.logger.warn({ message: e.message, ip: req.ip });
-      throw new Unauthorized();
-    }
-
-    const accessToken = await this.authService.signJwt({ id: payload.id, role: payload.role, exp: this._ACCESS_TOKEN_EXPIRE_TIME_IN_SECONDS });
-    res.cookie('acc', accessToken, this._getTokenCookieOptions(TokenType.ACCESS));
-
-    const needRefreshTokenRenewal = this.authService.reachRefreshTokenRenewalPeriod(payload.exp, new Date());
-    if (needRefreshTokenRenewal) {
-      const renewalRefreshToken = await this.authService.signJwt({ id: payload.id, role: payload.role, exp: this._REFRESH_TOKEN_EXPIRE_TIME_IN_SECONDS }, TokenType.REFRESH);
-      await this.authService.discardToken(refreshToken);
-      res.cookie('refr', renewalRefreshToken, this._getTokenCookieOptions(TokenType.REFRESH));
-    }
-
     return true;
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,31 +2,39 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
-import { AdminService } from '@src/admin/admin.service';
-import { AdminRoleType } from '@src/admin/entity.ts/admin.entity';
-import { Unauthorized } from '@src/common/exception/definition.exception';
-import { HashService } from '@src/common/utils/hash';
-import { DiscardedToken } from './entity/discardedToken.entity';
 import { Repository } from 'typeorm';
-import { BannedIp } from './entity/banned-ip.entity';
 
-export enum TokenType {
+import { MyLogger } from '@src/configs/logger/my-logger';
+import { Forbidden, Unauthorized } from '@src/common/exception/definition.exception';
+
+import { AdminService } from '@src/admin/admin.service';
+import { BannedIp } from './entity/banned-ip.entity';
+import { DiscardedToken } from './entity/discardedToken.entity';
+import { FailedSigninAttemptsCache } from './failed-signin-attempts-cache';
+import { HashService } from '@src/common/utils/hash';
+import { Admin, AdminRoleType } from '@src/admin/entity.ts/admin.entity';
+
+enum TokenType {
   ACCESS = 'access',
   REFRESH = 'refresh',
 }
-export type JwtPayload = { id: number; role: AdminRoleType; exp: number; fingerprint: string } & Record<string, any>;
+type JwtPayload = { id: number; role: AdminRoleType; exp: number; fingerprint: string } & Record<string, any>;
 
 @Injectable()
 export class AuthService {
   private readonly _JWT_SECRET: string;
   private readonly _JWT_REFRESH_SECRET: string;
+  private readonly _ACCESS_TOKEN_EXPIRE_TIME_IN_SECONDS: number;
+  private readonly _REFRESH_TOKEN_EXPIRE_TIME_IN_SECONDS: number;
   private readonly _REFRESH_TOKEN_RENEWAL_PERIOD_IN_SECONDS: number;
 
   constructor(
-    private readonly jwtService: JwtService,
+    private readonly logger: MyLogger,
     private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
     private readonly adminService: AdminService,
     private readonly hashService: HashService,
+    private readonly failedSigninAttemptsCache: FailedSigninAttemptsCache,
     @InjectRepository(DiscardedToken)
     private readonly discardedTokenRepository: Repository<DiscardedToken>,
     @InjectRepository(BannedIp)
@@ -34,6 +42,8 @@ export class AuthService {
   ) {
     this._JWT_SECRET = this.configService.get('SM_JWT_SECRET');
     this._JWT_REFRESH_SECRET = this.configService.get('SM_JWT_REFRESH_SECRET');
+    this._ACCESS_TOKEN_EXPIRE_TIME_IN_SECONDS = this.configService.get('SM_JWT_ACCESS_EXPIRE_TIME');
+    this._REFRESH_TOKEN_EXPIRE_TIME_IN_SECONDS = this.configService.get('SM_JWT_REFRESH_EXPIRE_TIME');
     this._REFRESH_TOKEN_RENEWAL_PERIOD_IN_SECONDS = this.configService.get('SM_JWT_REFRESH_TOKEN_RENEWAL_PERIOD');
   }
 
@@ -44,13 +54,13 @@ export class AuthService {
     return payload;
   }
 
-  async signJwt(payload: Record<string, any>, tokenType: TokenType = TokenType.ACCESS): Promise<string> {
+  private async _signJwt(payload: Record<string, any>, tokenType: TokenType = TokenType.ACCESS): Promise<string> {
     return this.jwtService.signAsync(payload, {
       secret: tokenType === TokenType.ACCESS ? this._JWT_SECRET : this._JWT_REFRESH_SECRET,
     });
   }
 
-  async authenticate(email: string, password: string) {
+  private async _authenticate(email: string, password: string) {
     const admin = await this.adminService.getAdminByEmailOrThrow(email);
     const isPasswordCorrect = await this.hashService.compare(password, admin.password);
     if (!isPasswordCorrect) throw new Unauthorized();
@@ -58,17 +68,58 @@ export class AuthService {
     return admin;
   }
 
-  async discardToken(token: string) {
-    const discardedToken = DiscardedToken.of({ token, createdAt: new Date() });
-    return this.discardedTokenRepository.save(discardedToken);
+  async signin({ email, password, ip, fingerprint }) {
+    const failedAttempts = await this.failedSigninAttemptsCache.get(email);
+    if (failedAttempts >= 5) throw new Forbidden('Too many failed login attempts');
+
+    let admin: Admin;
+    try {
+      admin = await this._authenticate(email, password);
+    } catch (e) {
+      this.logger.warn({ message: 'authentication failed', ip });
+      await this.failedSigninAttemptsCache.set(email, (failedAttempts || 0) + 1);
+      throw e;
+    }
+
+    const [accessToken, refreshToken] = await Promise.all([
+      this._signJwt({
+        id: admin.id,
+        role: admin.role,
+        exp: this._ACCESS_TOKEN_EXPIRE_TIME_IN_SECONDS,
+        fingerprint,
+      }),
+      this._signJwt(
+        {
+          id: admin.id,
+          role: admin.role,
+          exp: this._REFRESH_TOKEN_EXPIRE_TIME_IN_SECONDS,
+          fingerprint,
+        },
+        TokenType.REFRESH,
+      ),
+    ]);
+
+    if (failedAttempts > 0) await this.failedSigninAttemptsCache.clear(email);
+    return {
+      admin,
+      accessTokenInfo: {
+        token: accessToken,
+        exp: this._ACCESS_TOKEN_EXPIRE_TIME_IN_SECONDS,
+      },
+      refreshTokenInfo: {
+        token: refreshToken,
+        exp: this._REFRESH_TOKEN_EXPIRE_TIME_IN_SECONDS,
+      },
+    };
   }
 
-  async isDiscardedToken(token: string) {
-    const discardedToken = await this.discardedTokenRepository.findOne({ where: { token } });
-    return !!discardedToken;
+  private _reachRefreshTokenRenewalPeriod(exp: number, now: Date) {
+    const refreshTokenExpiry = new Date(exp * 1000);
+    const timeDiffInSeconds = (refreshTokenExpiry.getTime() - now.getTime()) / 1000;
+    return timeDiffInSeconds <= this._REFRESH_TOKEN_RENEWAL_PERIOD_IN_SECONDS;
   }
 
-  async banIp(ip: string) {
+  private async _banIp(ip: string) {
     const bannedIp = new BannedIp();
     bannedIp.ip = ip;
     return this.bannedIpRepository.save(bannedIp);
@@ -79,10 +130,60 @@ export class AuthService {
     return !!bannedIp;
   }
 
-  reachRefreshTokenRenewalPeriod(exp: number, now: Date) {
-    const refreshTokenExpiry = new Date(exp * 1000);
-    const timeDiffInSeconds = (refreshTokenExpiry.getTime() - now.getTime()) / 1000;
+  async discardToken(token: string) {
+    const discardedToken = DiscardedToken.of({ token, createdAt: new Date() });
+    return this.discardedTokenRepository.save(discardedToken);
+  }
 
-    return timeDiffInSeconds <= this._REFRESH_TOKEN_RENEWAL_PERIOD_IN_SECONDS;
+  private async _isDiscardedToken(token: string) {
+    const discardedToken = await this.discardedTokenRepository.findOne({ where: { token } });
+    return !!discardedToken;
+  }
+
+  async refresh({ refreshToken, ip, fingerprint }) {
+    if (await this._isDiscardedToken(refreshToken)) {
+      this.logger.warn({ message: 'refresh token is discarded', ip });
+      await this._banIp(ip);
+      throw new Forbidden();
+    }
+
+    let payload: JwtPayload;
+    try {
+      payload = await this.verifyJwt(refreshToken, TokenType.REFRESH);
+      if (payload.fingerprint !== fingerprint) throw Error('fingerprint mismatch');
+    } catch (e) {
+      this.logger.warn({ message: e.message, ip });
+      throw new Unauthorized();
+    }
+
+    const accessToken = await this._signJwt({
+      id: payload.id,
+      role: payload.role,
+      exp: this._ACCESS_TOKEN_EXPIRE_TIME_IN_SECONDS,
+    });
+
+    if (this._reachRefreshTokenRenewalPeriod(payload.exp, new Date())) {
+      refreshToken = await this._signJwt(
+        {
+          id: payload.id,
+          role: payload.role,
+          exp: this._REFRESH_TOKEN_EXPIRE_TIME_IN_SECONDS,
+          fingerprint,
+        },
+        TokenType.REFRESH,
+      );
+      await this.discardToken(refreshToken);
+    }
+
+    return {
+      accessTokenInfo: {
+        token: accessToken,
+        exp: this._ACCESS_TOKEN_EXPIRE_TIME_IN_SECONDS,
+      },
+      refreshTokenInfo: {
+        token: refreshToken,
+        exp: this._REFRESH_TOKEN_EXPIRE_TIME_IN_SECONDS,
+      },
+    };
   }
 }
