f1d7dd332564223d4fd563492d598c9aa73015d6
seungwoo <jsu7375@gmail.com>
2025-03-27 00:54:44 +0900

    FEAT: 각 enum들의 키값 타입 생성

--- src/common/constant/common.const.ts
+++ src/common/constant/common.const.ts
@@ -2,3 +2,11 @@ export enum Gender {
   MALE = 'M',
   FEMALE = 'F',
 }
+export type GenderKeyType = keyof typeof Gender;
+
+export enum SchoolLevel {
+  ELEMENTARY = '초',
+  MIDDLE = '중',
+  HIGH = '고',
+}
+export type SchoolLevelKeyType = keyof typeof SchoolLevel;
