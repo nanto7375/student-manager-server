d51882f09aacb77fefe082f20d3aeeccf8b43051
seungwoo <jsu7375@gmail.com>
2025-03-27 00:55:05 +0900

    FEAT: student-builder의 메소드 매개변수 타입 지정

--- src/student/student.builder.ts
+++ src/student/student.builder.ts
@@ -1,5 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { Student } from './entity/student.entity';
+import { Gender, GenderKeyType, SchoolLevel, SchoolLevelKeyType } from '@src/common/constant/common.const';
+import { ClassSchedule } from '@src/schedule/entity/class-schedule.entity';
+
+type CreatorSetBirth = {
+  birthYear: string;
+  birthDate: string;
+  gender: GenderKeyType;
+};
+type CreatorSetPhone = {
+  phone: string;
+  parentPhone: string;
+};
+type CreatorSetSchool = {
+  schoolName: string;
+  schoolLevel: SchoolLevelKeyType;
+};
+type CreatorSetClass = {
+  classSchedule: ClassSchedule;
+  tuition: number;
+};
+
+type EditorSetPhone = {
+  phone: string;
+  parentPhone: string;
+};
+type EditorSetSchool = {
+  schoolName: string;
+  schoolLevel: SchoolLevelKeyType;
+};
+type EditorSetClass = {
+  classSchedule: ClassSchedule;
+  tuition: number;
+};
 
 class StudentCreator {
   private _student: Student;
@@ -9,27 +42,28 @@ class StudentCreator {
     this._student.name = name;
   }
 
-  setBirth({ birthYear, birthDate, gender }) {
+  setBirth({ birthYear, birthDate, gender }: CreatorSetBirth) {
     this._student.birthYear = birthYear;
     this._student.birthDate = birthDate;
-    this._student.gender = gender;
+    this._student.gender = Gender[gender];
     return this;
   }
-  setPhone({ phone, parentPhone }) {
+  setPhone({ phone, parentPhone }: CreatorSetPhone) {
     this._student.phone = phone;
     this._student.parentPhone = parentPhone;
     return this;
   }
-  setSchool({ schoolName }) {
+  setSchool({ schoolName, schoolLevel }: CreatorSetSchool) {
     this._student.schoolName = schoolName;
+    this._student.schoolLevel = SchoolLevel[schoolLevel];
     return this;
   }
-  setClass({ classSchedule, tuition }) {
+  setClass({ classSchedule, tuition }: CreatorSetClass) {
     this._student.classSchedule = classSchedule;
     this._student.tuition = tuition;
     return this;
   }
-  build() {
+  create() {
     return this._student;
   }
 }
@@ -37,22 +71,23 @@ class StudentCreator {
 class StudentEditor {
   constructor(private readonly _student: Student) {}
 
-  setPhone({ phone, parentPhone }) {
+  setPhone({ phone, parentPhone }: EditorSetPhone) {
     if (phone) this._student.phone = phone;
     if (parentPhone) this._student.parentPhone = parentPhone;
     return this;
   }
-  setSchool({ schoolName }) {
+  setSchool({ schoolName, schoolLevel }: EditorSetSchool) {
     if (schoolName) this._student.schoolName = schoolName;
+    if (schoolLevel) this._student.schoolLevel = SchoolLevel[schoolLevel];
     return this;
   }
-  setClass({ classSchedule, tuition }) {
+  setClass({ classSchedule, tuition }: EditorSetClass) {
     if (classSchedule) this._student.classSchedule = classSchedule;
     if (tuition) this._student.tuition = tuition;
     return this;
   }
   edit() {
-    return this;
+    return this._student;
   }
 }
 
