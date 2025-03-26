716f450782bcc434356404408740f8b65c23c63d
seungwoo <jsu7375@gmail.com>
2025-03-26 23:28:13 +0900

    FEAT: 수업 스케쥴 모델 작성

---
+++ src/schedule/entity/class-schedule.entity.ts
@@ -0,0 +1,40 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+export enum DayOfWeek {
+  MONDAY = 1,
+  TUESDAY = 2,
+  WEDNESDAY = 3,
+  THURSDAY = 4,
+  FRIDAY = 5,
+  SATURDAY = 6,
+  SUNDAY = 7,
+}
+
+@Entity()
+export class ClassSchedule {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  dayOfWeek: number;
+
+  @Column()
+  startTime: Date;
+
+  @Column()
+  endTime: Date;
+
+  static of(classScheduleDto: ClassScheduleOf) {
+    const classSchedule = new ClassSchedule();
+    classSchedule.dayOfWeek = classScheduleDto.dayOfWeek;
+    classSchedule.startTime = classScheduleDto.startTime;
+    classSchedule.endTime = classScheduleDto.endTime;
+    return classSchedule;
+  }
+}
+
+type ClassScheduleOf = {
+  dayOfWeek: DayOfWeek;
+  startTime: Date;
+  endTime: Date;
+};
