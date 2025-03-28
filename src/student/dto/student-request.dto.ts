5a50898ff3d8206caa8dc13dce94cd0596972d08
seungwoo <jsu7375@gmail.com>
2025-03-29 02:14:17 +0900

    FEAT: 학생 등록 요청 dto에 validation 추가

--- src/student/dto/student-request.dto.ts
+++ src/student/dto/student-request.dto.ts
@@ -1,37 +1,66 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Gender, SchoolLevelKey } from '@src/common/constant/common.const';
+import { Gender, SchoolLevel } from '@src/common/constant/common.const';
+import { IsDate, IsEnum, IsNotEmpty, IsNumber, IsString, MaxLength, Min, MinLength } from 'class-validator';
 
 export class RegisterStudentRequestDto {
   @ApiProperty({ description: '이름' })
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(1)
+  @MaxLength(30)
   name: string;
 
+  @ApiProperty({ description: '수업 시간' })
+  @IsNumber()
+  @IsNotEmpty()
+  classScheduleId: number;
+
+  @ApiProperty({ description: '수업료' })
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(100_000)
+  tuition: number;
+
   @ApiProperty({ description: '생년' })
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(4)
+  @MaxLength(4)
   birthYear: string;
 
-  @ApiProperty({ description: '생년월일', nullable: true })
+  @ApiProperty({ description: '생월일', nullable: true })
+  @IsString()
+  @MinLength(4)
+  @MaxLength(4)
   birthDate: string;
 
   @ApiProperty({ description: '성별', nullable: true })
+  @IsEnum(Gender)
   gender: Gender;
 
-  @ApiProperty({ description: '수업료', nullable: true })
-  tuition: number;
-
   @ApiProperty({ description: '전화번호', nullable: true })
+  @IsString()
+  @MinLength(12)
+  @MaxLength(13)
   phone: string;
 
   @ApiProperty({ description: '부모 전화번호', nullable: true })
+  @IsString()
+  @MinLength(12)
+  @MaxLength(13)
   parentPhone: string;
 
   @ApiProperty({ description: '학교명', nullable: true })
+  @IsString()
+  @MinLength(1)
+  @MaxLength(30)
   schoolName: string;
 
   @ApiProperty({ description: '학교 레벨', nullable: true })
-  schoolLevel: SchoolLevelKey;
-
-  @ApiProperty({ description: '수업 시간', nullable: true })
-  classScheduleId: number;
+  @IsEnum(SchoolLevel)
+  schoolLevel: SchoolLevel;
 
   @ApiProperty({ description: '등록일', nullable: true })
+  @IsDate()
   registeredAt: Date;
 }
