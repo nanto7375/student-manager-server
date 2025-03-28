7710c119f6f44eacbd33f7fe03336a69335a36fe
seungwoo <jsu7375@gmail.com>
2025-03-29 02:12:12 +0900

    FEAT: strict null checks - false

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -42,6 +42,7 @@ export default tseslint.config(
       'no-empty': 'off',
       '@typescript-eslint/restrict-plus-operands': 'off',
       '@typescript-eslint/require-await': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
     },
   },
 );
