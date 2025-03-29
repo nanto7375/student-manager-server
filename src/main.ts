9a72a331f60f641218e94c13a4aff0bfb57f0fd4
seungwoo <jsu7375@gmail.com>
2025-03-29 13:26:17 +0900

    FEAT: global validation pipe에 custom exceptionFactory 적용

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,8 @@ import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { logger } from './configs/logger/winston-logger';
 import { Environment } from './configs/config.service';
+import { BadRequest } from './common/exception/definition.exception';
+import { ValidationError } from 'class-validator';
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
@@ -27,13 +29,29 @@ const setSwagger = (app: NestExpressApplication) => {
   SwaggerModule.setup(`api`, app, swaggerDocument, { swaggerOptions: { defaultModelExpandDepth: 5, defaultModelsExpandDepth: 5 } });
 };
 
+const _exceptionFactory = (errors: ValidationError[]) => {
+  console.log('--------------------------------');
+  errors.forEach((error) => {
+    console.log({
+      tartget: error.target.constructor.name,
+      property: error.property,
+      constraints: error.constraints,
+    });
+  });
+  console.log('--------------------------------');
+  return new BadRequest();
+};
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     bodyParser: true,
     cors: true,
     logger: logger,
   });
+
   const configService = app.get(ConfigService);
+  const env = configService.get('SM_ENV');
+  if (env !== Environment.Production) setSwagger(app);
 
   const serverVersion = configService.get('SM_SERVER_VERSION');
   app.enableVersioning({
@@ -41,9 +59,12 @@ async function bootstrap() {
     defaultVersion: serverVersion,
   });
   app.use(cookieParser());
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
-
-  if (configService.get('NAWS_ENV') !== Environment.Production) setSwagger(app);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      exceptionFactory: (errors) => (env === Environment.Production ? new BadRequest() : _exceptionFactory(errors)),
+    }),
+  );
 
   const port = configService.get('SM_PORT');
   await app.listen(port, () => {
