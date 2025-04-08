7a211a5151ef9d81766f847849a9b0353a4cdaa6
seungwoo <jsu7375@gmail.com>
2025-04-08 09:53:43 +0900

    REFACTOR: 학생 등록, 수정 validation이 dto에 있던 것을 builder로 이동

--- src/student/dto/student-request.dto.ts
+++ src/student/dto/student-request.dto.ts
@@ -1,13 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Gender, SchoolLevel } from '@src/common/constant/common.const';
-import { IsDate, IsEnum, IsNumber, IsString, MaxLength, Min, MinLength } from 'class-validator';
+import { IsDate, IsEnum, IsNumber, IsString } from 'class-validator';
 import { Nullable, Optional } from 'class-validator-extended';
 
 export class RegisterStudentRequestDto {
   @ApiProperty({ description: '이름' })
   @IsString()
-  @MinLength(1)
-  @MaxLength(30)
   name: string;
 
   @ApiProperty({ description: '수업 시간' })
@@ -16,20 +14,15 @@ export class RegisterStudentRequestDto {
 
   @ApiProperty({ description: '수업료' })
   @IsNumber()
-  @Min(100_000)
   tuition: number;
 
   @ApiProperty({ description: '생년' })
   @IsString()
-  @MinLength(4)
-  @MaxLength(4)
   birthYear: string;
 
   @ApiProperty({ description: '생월일', nullable: true })
   @Nullable()
   @IsString()
-  @MinLength(4)
-  @MaxLength(4)
   birthDate: string;
 
   @ApiProperty({ description: '성별', nullable: true })
@@ -40,22 +33,16 @@ export class RegisterStudentRequestDto {
   @ApiProperty({ description: '전화번호', nullable: true })
   @Nullable()
   @IsString()
-  @MinLength(12)
-  @MaxLength(13)
   phone: string;
 
   @ApiProperty({ description: '부모 전화번호', nullable: true })
   @Nullable()
   @IsString()
-  @MinLength(12)
-  @MaxLength(13)
   parentPhone: string;
 
   @ApiProperty({ description: '학교명', nullable: true })
   @Nullable()
   @IsString()
-  @MinLength(1)
-  @MaxLength(30)
   schoolName: string;
 
   @ApiProperty({ description: '학교 레벨', nullable: true })
@@ -78,28 +65,21 @@ export class PatchStudentRequestDto {
   @ApiProperty({ description: '수업료' })
   @Optional()
   @IsNumber()
-  @Min(100_000)
   tuition: number;
 
   @ApiProperty({ description: '전화번호', nullable: true })
   @Optional()
   @IsString()
-  @MinLength(12)
-  @MaxLength(13)
   phone: string;
 
   @ApiProperty({ description: '부모 전화번호', nullable: true })
   @Optional()
   @IsString()
-  @MinLength(12)
-  @MaxLength(13)
   parentPhone: string;
 
   @ApiProperty({ description: '학교명', nullable: true })
   @Optional()
   @IsString()
-  @MinLength(1)
-  @MaxLength(30)
   schoolName: string;
 
   @ApiProperty({ description: '학교 레벨', nullable: true })
--- src/student/student.builder.ts
+++ src/student/student.builder.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Student } from './entity/student.entity';
 import { Gender, SchoolLevel } from '@src/common/constant/common.const';
 import { ClassSchedule } from '@src/schedule/entity/class-schedule.entity';
+import { BadRequest } from '@src/common/exception/definition.exception';
 
 type CreatorSetBirth = {
   birthYear: string;
@@ -39,27 +40,41 @@ class StudentCreator {
   private _student: Student;
 
   constructor(name: string) {
+    if (name.length < 1 || name.length > 30) throw new BadRequest('wrong name');
+
     this._student = new Student();
     this._student.name = name;
   }
 
   setBirth({ birthYear, birthDate, gender }: CreatorSetBirth) {
+    if (birthYear.length !== 4) throw new BadRequest('wrong birthYear');
+    if (birthDate.length !== 4) throw new BadRequest('wrong birthDate');
+    if (!Object.values(Gender).includes(gender)) throw new BadRequest('wrong gender');
+
     this._student.birthYear = birthYear;
     this._student.birthDate = birthDate;
     this._student.gender = gender;
     return this;
   }
   setPhone({ phone, parentPhone }: CreatorSetPhone) {
+    if (phone.length < 9 || phone.length > 13) throw new BadRequest('wrong phone');
+    if (parentPhone.length < 9 || parentPhone.length > 13) throw new BadRequest('wrong parentPhone');
+
     this._student.phone = phone;
     this._student.parentPhone = parentPhone;
     return this;
   }
   setSchool({ schoolName, schoolLevel }: CreatorSetSchool) {
+    if (schoolName.length < 1 || schoolName.length > 30) throw new BadRequest('wrong schoolName');
+    if (!Object.values(SchoolLevel).includes(schoolLevel)) throw new BadRequest('wrong schoolLevel');
+
     this._student.schoolName = schoolName;
     this._student.schoolLevel = schoolLevel;
     return this;
   }
   setClass({ classSchedule, tuition, registeredAt }: CreatorSetClass) {
+    if (tuition < 100_000) throw new BadRequest('wrong tuition');
+
     this._student.classSchedule = classSchedule;
     this._student.tuition = tuition;
     this._student.registeredAt = registeredAt;
@@ -74,16 +89,24 @@ class StudentEditor {
   constructor(private readonly _student: Student) {}
 
   setPhone({ phone, parentPhone }: EditorSetPhone) {
+    if (phone && (phone.length < 9 || phone.length > 13)) throw new BadRequest('wrong phone');
+    if (parentPhone && (parentPhone.length < 9 || parentPhone.length > 13)) throw new BadRequest('wrong parentPhone');
+
     if (phone) this._student.phone = phone;
     if (parentPhone) this._student.parentPhone = parentPhone;
     return this;
   }
   setSchool({ schoolName, schoolLevel }: EditorSetSchool) {
+    if (schoolName && (schoolName.length < 1 || schoolName.length > 30)) throw new BadRequest('wrong schoolName');
+    if (schoolLevel && !Object.values(SchoolLevel).includes(schoolLevel)) throw new BadRequest('wrong schoolLevel');
+
     if (schoolName) this._student.schoolName = schoolName;
     if (schoolLevel) this._student.schoolLevel = schoolLevel;
     return this;
   }
   setClass({ classSchedule, tuition }: EditorSetClass) {
+    if (tuition && tuition < 100_000) throw new BadRequest('wrong tuition');
+
     if (classSchedule) this._student.classSchedule = classSchedule;
     if (tuition) this._student.tuition = tuition;
     return this;
