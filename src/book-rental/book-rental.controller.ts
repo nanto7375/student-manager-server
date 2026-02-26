6a4ccbb2fa6f1aa136be988f661c5d1a343f4300
apmJSW <jeongsw@apm-coin.com>
2026-02-26 18:51:37 +0900

    FEAT: book-rental api path 수정

--- src/book-rental/book-rental.controller.ts
+++ src/book-rental/book-rental.controller.ts
@@ -1,16 +1,17 @@
 import { Body, Controller, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
-import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { toInstance } from '@src/common/utils/toInstance';
 import { BorrowBookRequestDto, UpdateBookRentalRequestDto } from './dto/book-rental.request.dto';
 import { BookRentalService } from './book-rental.service';
 import { BookRentalDto } from './dto/book-rental.response.dto';
 
-@Controller('book-rental')
+@ApiTags('book-rental')
+@Controller('book-rentals')
 export class BookRentalController {
   constructor(private readonly bookRentalService: BookRentalService) {}
 
-  @Post('book-rentals')
+  @Post()
   @ApiOperation({ summary: '도서 대출 기록 생성' })
   @ApiOkResponse({ type: BookRentalDto })
   async createBookRentalActivityRecord(@Body() { studentId, bookTitle }: BorrowBookRequestDto) {
@@ -18,7 +19,7 @@ export class BookRentalController {
     return toInstance(BookRentalDto, bookRental);
   }
 
-  @Patch('book-rentals/:bookRentalId')
+  @Patch(':bookRentalId')
   @ApiOperation({ summary: '대출 도서 정보 업데이트' })
   @ApiOkResponse({ type: BookRentalDto })
   async updateBookRentalActivityRecord(
@@ -29,7 +30,7 @@ export class BookRentalController {
     return toInstance(BookRentalDto, bookRental);
   }
 
-  @Patch('book-rentals/:bookRentalId/return')
+  @Patch(':bookRentalId/return')
   @ApiOperation({ summary: '도서 반납 처리' })
   @ApiOkResponse({ type: Boolean })
   async returnBook(@Param('bookRentalId', ParseIntPipe) bookRentalId: number) {
