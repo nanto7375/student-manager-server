3c611d6e3b598b27f18da65ce2985ee10e8eb999
apmJSW <jeongsw@apm-coin.com>
2026-04-20 18:46:37 +0900

    style: arrange code

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -15,7 +15,6 @@ import { AdminDto } from '@src/admin/dto/admin-response.dto';
 import { SigninResponseDto } from './dto/auth-response.dto';
 
 // TODO: auth용 throttler 따로 설정하기
-// TODO: ip ban 처리 미들웨어로 따로 뺄까?
 @Controller('auth')
 @ApiTags('auth')
 @UseGuards(ThrottlerGuard)
